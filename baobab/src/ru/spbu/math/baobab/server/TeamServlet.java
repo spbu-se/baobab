366652715fea2d12e9518a4b6f25625431bf6b4d
Vita.Loginova <Vita.Loginova@localhost>
2012-10-16 10:02:48 +0400

    dbarashev => vloginova review: dluciv, dbarashev

---
+++ baobab/src/ru/spbu/math/baobab/server/TeamServlet.java
@@ -0,0 +1,22 @@
+package ru.spbu.math.baobab.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class TeamServlet extends HttpServlet {
+  protected String [] myMembers = new String [] {
+	"Dmitry Barashev", "Anton Bondarev", "Dmitry Vadimovich Luciv", "Alexander Gudulin",
+	"Ageev Denis", "Alexandr Lapitckiy", "Vita Loginova", "Sergey Morozov"
+  };
+	
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.getWriter().println("The Team:");
+    for(String member : myMembers)
+      resp.getWriter().println(" * " + member);
+  }
+}
