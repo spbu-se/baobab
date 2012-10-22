55f5ae3c5169ba4f3814a29e2927eeb6b29cd787
dnsjkee <dnsjkee@localhost>
2012-10-22 18:30:30 +0400

    In-memory implementation of TimeSlot

---
+++ baobab/src/ru/spbu/math/baobab/server/TimeSlotImpl.java
@@ -0,0 +1,56 @@
+package ru.spbu.math.baobab.server;
+
+import ru.spbu.math.baobab.model.EvenOddWeek;
+import ru.spbu.math.baobab.model.TimeInstant;
+import ru.spbu.math.baobab.model.TimeSlot;
+import ru.spbu.math.baobab.model.TimeSlotExtent;
+
+/**
+ * Implementation of TimeSlot interface
+ * 
+ * @author dageev
+ */
+
+public class TimeSlotImpl implements TimeSlot {
+  private String mName;
+  private int mDay;
+  private EvenOddWeek mFlashing;
+  private TimeInstant mStart;
+  private TimeInstant mFinish;
+  private TimeSlotExtent mTimeSlotExtent;
+  
+  
+  public TimeSlotImpl(String name, TimeInstant start, TimeInstant finish,
+			          int day, EvenOddWeek flashing, TimeSlotExtent timeslotextent){
+	mTimeSlotExtent = timeslotextent;
+	mName=name;
+	mDay=day; 
+	mFlashing= flashing;
+	mStart = start;
+	mFinish= finish;
+  }
+  
+  public String getName(){
+    return mName;
+  }
+
+  public int getDayOfWeek(){
+    return mDay;
+  }
+
+  public EvenOddWeek getEvenOddWeek(){
+	return mFlashing;
+  }
+  
+  public TimeInstant getStart(){
+	return mStart;
+  }
+
+  public TimeInstant getFinish(){
+	return mFinish;
+  }
+  
+  public TimeSlotExtent getExtent(){
+	return mTimeSlotExtent;
+  }
+}
