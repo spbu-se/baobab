be1dd6fa6634f290ae0f2d8198ba7761cfbec780
dnsjkee <dnsjkee@localhost>
2012-10-23 16:15:13 +0400

    Fixfixfixfix. Create testGetAll.
    review: dbarashev

--- baobab/src/ru/spbu/math/baobab/server/TimeSlotExtentImpl.java
+++ baobab/src/ru/spbu/math/baobab/server/TimeSlotExtentImpl.java
@@ -13,24 +13,25 @@ import ru.spbu.math.baobab.model.EvenOddWeek;
 import ru.spbu.math.baobab.model.TimeInstant;
 import ru.spbu.math.baobab.model.TimeSlot;
 import ru.spbu.math.baobab.model.TimeSlotExtent;
+
 /**
  * Implementation of TimeSlotExtent interface
  * 
  * @author dageev
  */
-public class TimeSlotExtentImpl implements TimeSlotExtent{
+public class TimeSlotExtentImpl implements TimeSlotExtent {
   private final Collection<TimeSlot> myTimeSlots = Lists.newArrayList();
-  
+
   @Override
-  public Collection<TimeSlot> getAll(){
+  public Collection<TimeSlot> getAll() {
     return myTimeSlots;
   }
 
   @Override
-  public List<TimeSlot> findByWeekDay(int day){
+  public List<TimeSlot> findByWeekDay(int day) {
     List<TimeSlot> list = Lists.newArrayList();
-    for (TimeSlot ts : myTimeSlots){
-      if (ts.getDayOfWeek() == day){
+    for (TimeSlot ts : myTimeSlots) {
+      if (ts.getDayOfWeek() == day) {
         list.add(ts);
       }
     }
@@ -39,25 +40,25 @@ public class TimeSlotExtentImpl implements TimeSlotExtent{
   }
 
   @Override
-  public List<TimeSlot> findByDate(Date date){
+  public List<TimeSlot> findByDate(Date date) {
     List<TimeSlot> list = Lists.newArrayList();
     Calendar calendar = GregorianCalendar.getInstance();
     calendar.setTime(date);
     int day = calendar.get(Calendar.DAY_OF_WEEK);
     boolean isEven = false;
-    if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 1){
+    if (calendar.get(Calendar.WEEK_OF_YEAR) % 2 == 1) {
       isEven = true;
     }
-    for (TimeSlot ts : myTimeSlots){
-      if (ts.getDayOfWeek() == day){
+    for (TimeSlot ts : myTimeSlots) {
+      if (ts.getDayOfWeek() == day) {
         boolean add = true;
-        if (ts.getEvenOddWeek() == EvenOddWeek.ODD && !isEven){
+        if (ts.getEvenOddWeek() == EvenOddWeek.ODD && !isEven) {
           add = false;
         }
-        if (ts.getEvenOddWeek() == EvenOddWeek.EVEN && isEven){
+        if (ts.getEvenOddWeek() == EvenOddWeek.EVEN && isEven) {
           add = false;
         }
-        if (add){
+        if (add) {
           list.add(ts);
         }
       }
@@ -67,31 +68,26 @@ public class TimeSlotExtentImpl implements TimeSlotExtent{
   }
 
   @Override
-  public TimeSlot create(String name, TimeInstant start, TimeInstant finish,
-      int day, EvenOddWeek flashing){
-    for (TimeSlot ts : myTimeSlots){
-      if (ts.getName() == name){
-        throw new IllegalStateException(
-            "The TimeSlot with this name is already exist"); 
+  public TimeSlot create(String name, TimeInstant start, TimeInstant finish, int day, EvenOddWeek flashing) {
+    for (TimeSlot ts : myTimeSlots) {
+      if (ts.getName().equals(name)) {
+        throw new IllegalStateException("The TimeSlot with this name is already exist");
       }
     }
-    TimeSlot timeslot = new TimeSlotImpl(name, start, finish, day, flashing,
-        this);
+    TimeSlot timeslot = new TimeSlotImpl(name, start, finish, day, flashing, this);
     myTimeSlots.add(timeslot);
     return timeslot;
 
   }
 
-  private static Comparator<TimeSlot> DATE_COMPARATOR = new Comparator<TimeSlot>(){
+  private static Comparator<TimeSlot> DATE_COMPARATOR = new Comparator<TimeSlot>() {
     @Override
-    public int compare(TimeSlot o1, TimeSlot o2){
+    public int compare(TimeSlot o1, TimeSlot o2) {
       TimeInstant timeStart1 = o1.getStart();
       TimeInstant timeStart2 = o2.getStart();
       int val1 = timeStart1.getHour() * 60 + timeStart1.getMinute();
       int val2 = timeStart2.getHour() * 60 + timeStart2.getMinute();
       return (val1 - val2);
     }
-
   };
-
 }
--- baobab/src/ru/spbu/math/baobab/server/TimeSlotExtentImplTest.java
+++ baobab/src/ru/spbu/math/baobab/server/TimeSlotExtentImplTest.java
@@ -1,72 +1,92 @@
 package ru.spbu.math.baobab.server;
 
-
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import ru.spbu.math.baobab.model.EvenOddWeek;
 import ru.spbu.math.baobab.model.TimeInstant;
 import ru.spbu.math.baobab.model.TimeSlot;
 import ru.spbu.math.baobab.model.TimeSlotExtent;
 import junit.framework.TestCase;
 
-public class TimeSlotExtentImplTest extends TestCase{
-  
+/**
+ * Some Tests for TimeSlotExtent
+ * 
+ * @author dageev
+ */
+public class TimeSlotExtentImplTest extends TestCase {
+
+  public void testGetAll() {
+    TimeInstant start = new TimeInstant(9, 30);
+    TimeInstant finish = new TimeInstant(11, 5);
+    TimeInstant start1 = new TimeInstant(13, 40);
+    TimeInstant finish1 = new TimeInstant(15, 15);
+    TimeSlotExtent timeSlotExtent = new TimeSlotExtentImpl();
+    TimeSlot ts1 = timeSlotExtent.create("first double class", start, finish, 2, EvenOddWeek.ALL);
+    TimeSlot ts2 = timeSlotExtent.create("second double class", start1, finish1, 2, EvenOddWeek.ALL);
+    TimeSlot ts3 = timeSlotExtent.create("third double class", start1, finish1, 2, EvenOddWeek.ALL);
+    Collection<TimeSlot> timeSlots = Lists.newArrayList();
+    timeSlots.add(ts1);
+    timeSlots.add(ts2);
+    timeSlots.add(ts3);
+    assertEquals(timeSlots, timeSlotExtent.getAll());
+  }
+
   public void testCreate() {
     TimeSlotExtent timeSlotExtent = new TimeSlotExtentImpl();
-    TimeInstant start = new TimeInstant(9,30);
-    TimeInstant finish = new TimeInstant(11,5);
+    TimeInstant start = new TimeInstant(9, 30);
+    TimeInstant finish = new TimeInstant(11, 5);
     timeSlotExtent.create("first double class", start, finish, 2, EvenOddWeek.ALL);
-    try{ 
-      TimeInstant start1 = new TimeInstant(13,40);
-      TimeInstant finish1 = new TimeInstant(15,15);
+    TimeInstant start1 = new TimeInstant(13, 40);
+    TimeInstant finish1 = new TimeInstant(15, 15);
+    try {
       timeSlotExtent.create("first double class", start1, finish1, 5, EvenOddWeek.ODD);
       fail("Expected IllegalArgumentException");
-    }
-    catch (IllegalArgumentException e){
+    } catch (IllegalArgumentException e) {
       // can't create with existing name
     }
   }
-  
-  public void testfindByWeekDay(){
+
+  public void testFindByWeekDay() {
     TimeSlotExtent timeSlotExtent = new TimeSlotExtentImpl();
-    TimeInstant start = new TimeInstant(9,30);
-    TimeInstant finish = new TimeInstant(11,5);
-    TimeInstant start1 = new TimeInstant(13,40);
-    TimeInstant finish1 = new TimeInstant(15,15);
-    timeSlotExtent.create("first double class1", start, finish, 2, EvenOddWeek.ALL);
-    timeSlotExtent.create("first double class2", start, finish, 3, EvenOddWeek.ALL);
-    timeSlotExtent.create("first double class3", start1, finish1, 4, EvenOddWeek.ALL);
-    timeSlotExtent.create("first double class4", start, finish, 4, EvenOddWeek.ALL);
+    TimeInstant start = new TimeInstant(9, 30);
+    TimeInstant finish = new TimeInstant(11, 5);
+    TimeInstant start1 = new TimeInstant(13, 40);
+    TimeInstant finish1 = new TimeInstant(15, 15);
+    timeSlotExtent.create("first double class", start, finish, 2, EvenOddWeek.ALL);
+    timeSlotExtent.create("second double class", start, finish, 3, EvenOddWeek.ALL);
+    timeSlotExtent.create("third double class", start1, finish1, 4, EvenOddWeek.ALL);
+    timeSlotExtent.create("fourth double class", start, finish, 4, EvenOddWeek.ALL);
     List<TimeSlot> list = timeSlotExtent.findByWeekDay(4);
     assertEquals(list.get(0).getName(), "first double class4");
     assertEquals(list.get(1).getName(), "first double class3");
   }
-  
-  public void testfindByDate(){
+
+  public void testFindByDate() {
     TimeSlotExtent timeSlotExtent = new TimeSlotExtentImpl();
-    TimeInstant start = new TimeInstant(9,30);
-    TimeInstant finish = new TimeInstant(11,5);
-    TimeInstant start1 = new TimeInstant(13,40);
-    TimeInstant finish1 = new TimeInstant(15,15);
+    TimeInstant start = new TimeInstant(9, 30);
+    TimeInstant finish = new TimeInstant(11, 5);
+    TimeInstant start1 = new TimeInstant(13, 40);
+    TimeInstant finish1 = new TimeInstant(15, 15);
     Calendar c = new GregorianCalendar();
-    c.setTimeInMillis(System.currentTimeMillis());
+    c.set(Calendar.WEEK_OF_YEAR, 1);
+    c.set(Calendar.DAY_OF_WEEK, 4);
     Date date1 = c.getTime();
-    c.add(Calendar.WEEK_OF_YEAR, -1); 
+    c.set(Calendar.WEEK_OF_YEAR, 3);
     Date date2 = c.getTime();
-    int day = c.get(Calendar.DAY_OF_WEEK);
-    timeSlotExtent.create("first double class2", start1, finish1, day, EvenOddWeek.ALL);
-    timeSlotExtent.create("first double class3", start, finish, day, EvenOddWeek.EVEN);
-    timeSlotExtent.create("first double class4", start1, finish1,day, EvenOddWeek.ODD);
+    timeSlotExtent.create("first double class", start1, finish1, 4, EvenOddWeek.ALL);
+    timeSlotExtent.create("second double class", start, finish, 4, EvenOddWeek.EVEN);
+    timeSlotExtent.create("third double class", start1, finish1, 4, EvenOddWeek.ODD);
     List<TimeSlot> list = timeSlotExtent.findByDate(date1);
     List<TimeSlot> list1 = timeSlotExtent.findByDate(date2);
-    assertNotSame(list.get(0).getName(),list1.get(0).getName());
-  
-    
+    assertEquals(list, list1);
+    c.set(Calendar.WEEK_OF_YEAR, 2);
+    date1 = c.getTime();
+    list1 = timeSlotExtent.findByDate(date1);
+    assertFalse(list.equals(list1));
   }
-  
- 
-
 }
