7adb90196dd07e33d5e1e5c8516aa2942cfcdab0
alexandr.gudulin <alexandr.gudulin@localhost>
2012-10-26 02:24:54 +0400

    Send error in response. Removed extra whitespace line. review: dbarashev

--- baobab/src/ru/spbu/math/baobab/server/TestPdfServlet.java
+++ baobab/src/ru/spbu/math/baobab/server/TestPdfServlet.java
@@ -16,7 +16,8 @@ import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.pdf.PdfWriter;
 
 /**
- * Test of creating a PDF file. If everything is OK, browser will recognize it as PDF and will open a PDF viewer.
+ * Test of creating a PDF file. If everything is OK, browser will recognize it
+ * as PDF and will open a PDF viewer.
  * 
  * @author agudulin
  */
@@ -24,7 +25,8 @@ public class TestPdfServlet extends HttpServlet {
   private static final Logger LOGGER = Logger.getLogger("PdfService");
 
   @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
     resp.setContentType("application/pdf");
     Document document = new Document();
 
@@ -33,13 +35,13 @@ public class TestPdfServlet extends HttpServlet {
 
       document.open();
       Paragraph title = new Paragraph();
-      title.add(new Paragraph("Baobab", new Font(Font.FontFamily.COURIER, 24, Font.BOLD)));
+      title.add(new Paragraph("Baobab", new Font(Font.FontFamily.COURIER, 24,
+          Font.BOLD)));
       document.add(title);
       document.close();
     } catch (DocumentException e) {
       LOGGER.log(Level.WARNING, "Failed to create PDF", e);
-      resp.getWriter().println("Failure. " + e.getMessage());
+      resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
     }
   }
-
 }
