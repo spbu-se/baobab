f8b0bf6a2c1633cb655a0f47326fa04ffda7fdf8
Vita.Loginova <Vita.Loginova@localhost>
2012-10-28 18:52:28 +0400

    timeslot regex for russian added + test; bug with \k<name> fixed review: dluciv

--- baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
+++ baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
@@ -7,23 +7,33 @@ import java.util.regex.Pattern;
 
 public class ScriptInterpreter {
 
+	private final static String RUSSIAN_VERSION = "Rus";
+
+	//command names
 	private final static String DEFINE_TIMESLOT_NAME = "DefineTimeslot";
 	private final static String DEFINE_ATTENDEE_NAME = "DefineAttendee";
-	
-	private final static String ID_NAME = "ID";
-	private final static String TIME_NAME = "TIME";
-	private final static String EVEN_ODD_NAME = "EVEN";
-	private final static String WEEKDAY_NAME = "WEEKDAY";
-	
+
+	//group names
+	private final static String ID_NAME = "Id";
+	private final static String TIME_NAME = "Time";
+	private final static String EVEN_ODD_NAME = "EvenOdd";
+	private final static String WEEKDAY_NAME = "Weekday";
+
+	//group patterns
 	private final static String ID_PATTERN = "\\w+";
 	private final static String TIME_PATTERN = "\\d{1,2}:\\d{2}";
 	private final static String EVEN_ODD_PATTERN_ENG = "odd|even";
 	private final static String EVEN_ODD_PATTERN_RUS = "четный|нечетный";
 	private final static String WEEKDAY_PATTERN_ENG = "Monday|Tuesday|Wednesday|Thursday|Friday|Saturday|Sunday|Mo|Tu|We|Th|Fr|Sa|Su";
 	private final static String WEEKDAY_PATTERN_RUS = "понедельник|вторник|среда|четверг|пятница|суббота|воскресенье|пн|вт|ср|чт|пт|сб|вс";
-	
-	private final static String DEFINE_TIMESLOT = String.format("(?<%s>define\\stimeslot)\\s(?<%s>%s)\\s(?<%s>%s)\\sto\\s\\k<%s>\\son\\s(?<%s>%s)\\s(?<%s>%s)",
-			DEFINE_TIMESLOT_NAME, ID_NAME, ID_PATTERN, TIME_NAME, TIME_PATTERN, TIME_NAME, EVEN_ODD_NAME, EVEN_ODD_PATTERN_ENG, WEEKDAY_NAME, WEEKDAY_PATTERN_ENG);
+
+	//command patterns: for command in Russian adds a suffix RUSSIAN_VERSION, for repeated parameters adds its order number in a raw
+	private final static String DEFINE_TIMESLOT = String.format("((?<%s>define\\stimeslot)\\s(?<%s>%s)\\s(?<%s>%s)\\sto\\s(?<%s>%s)\\son\\s(?<%s>%s)\\s(?<%s>%s))",
+			DEFINE_TIMESLOT_NAME, ID_NAME, ID_PATTERN, TIME_NAME, TIME_PATTERN, TIME_NAME + "2", TIME_PATTERN, EVEN_ODD_NAME, EVEN_ODD_PATTERN_ENG, WEEKDAY_NAME, WEEKDAY_PATTERN_ENG)
+			+ String.format("|((?<%s>определить\\sинтервал)\\s(?<%s>%s)\\sот\\s(?<%s>%s)\\sдо\\s(?<%s>%s)\\sв\\s(?<%s>%s)\\s(?<%s>%s))",
+					DEFINE_TIMESLOT_NAME + RUSSIAN_VERSION, ID_NAME + RUSSIAN_VERSION, ID_PATTERN, TIME_NAME + RUSSIAN_VERSION, 
+					TIME_PATTERN, TIME_NAME + RUSSIAN_VERSION + "2", TIME_PATTERN, EVEN_ODD_NAME + RUSSIAN_VERSION, 
+					EVEN_ODD_PATTERN_RUS, WEEKDAY_NAME + RUSSIAN_VERSION, WEEKDAY_PATTERN_RUS);
 	private final static String DEFINE_ATTENDEE = "";
 
 	private static Pattern MAIN_PATTERN;
@@ -60,11 +70,16 @@ public class ScriptInterpreter {
 	 * 
 	 * @return: void
 	 */
-	private void execute(Matcher match) {
-		if (match.group(DEFINE_TIMESLOT_NAME) != null) {
+	private void execute(Matcher match) throws Exception {
+		if ((match.group(DEFINE_TIMESLOT_NAME) != null) ||
+				(match.group(DEFINE_TIMESLOT_NAME + RUSSIAN_VERSION) != null)){
 			defineTimeSlot(match);
 		} else if (match.group(DEFINE_ATTENDEE_NAME) != null) {
 			defineAttendee(match);
+		}else {
+			throw new Exception(
+					//something wrong here
+					"Exception: this command doesn't correspond to a pattern");
 		}
 	}
 	
