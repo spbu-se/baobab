96106129e3891a2a7bfaa6a65838c91c4c2945bb
Vita.Loginova <Vita.Loginova@localhost>
2012-10-28 18:52:28 +0400

    timeslot regex for russian added + test; bug with \k<name> fixed review: dluciv

--- baobab/test/ru/spbu/math/baobab/lang/ScriptInterpreterTest.java
+++ baobab/test/ru/spbu/math/baobab/lang/ScriptInterpreterTest.java
@@ -12,7 +12,8 @@ public class ScriptInterpreterTest extends TestCase {
 	public void testCommandCorrectness() {
 		ScriptInterpreter interpteter = new ScriptInterpreter();
 		try {
-			interpteter.process("define timeslot 1st_try 12:12 to 12:12 on even Monday");
+			interpteter.process("define timeslot 1st_try 12:12 to 12:40 on even Monday");
+			interpteter.process("определить интервал 1st_try от 12:12 до 12:13 в нечетный понедельник");
 		} catch (Exception e) {
 			fail("command's not correct");
 		}
