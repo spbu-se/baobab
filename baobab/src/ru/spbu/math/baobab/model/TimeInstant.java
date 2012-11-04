ed610aa5b34f13d66e211cd8a8f47b30806a6080
alexandr.gudulin <alexandr.gudulin@localhost>
2012-11-04 22:01:45 +0400

    Rewrote hashCode() and equals() methods with google methods help. Also cleaned up.
    review: dbarashev

--- baobab/src/ru/spbu/math/baobab/model/TimeInstant.java
+++ baobab/src/ru/spbu/math/baobab/model/TimeInstant.java
@@ -1,52 +1,47 @@
 package ru.spbu.math.baobab.model;
 
 /**
- * Time instant is a moment of a day, with minute precision. It repeats every day.
- * E.g. 9:30 is a time instant.
+ * Time instant is a moment of a day, with minute precision. It repeats every day. E.g. 9:30 is a time instant.
  * 
  * @author dbarashev
  */
 public class TimeInstant {
   private final int myHour;
   private final int myMinute;
-  
+
   public TimeInstant(int hour, int minute) {
     assert hour >= 0 && hour <= 23 : "invalid hour value=" + hour;
     assert minute >= 0 && minute <= 59 : "invalid minute value=" + minute;
     myHour = hour;
     myMinute = minute;
   }
-  
+
   public int getHour() {
     return myHour;
   }
-  
+
   public int getMinute() {
     return myMinute;
   }
 
   @Override
   public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + myHour;
-    result = prime * result + myMinute;
-    return result;
+    return com.google.common.base.Objects.hashCode(myHour, myMinute);
   }
 
   @Override
   public boolean equals(Object obj) {
-    if (this == obj)
+    if (this == obj) {
       return true;
-    if (obj == null)
+    }
+    if (obj == null) {
       return false;
-    if (getClass() != obj.getClass())
+    }
+    if (getClass() != obj.getClass()) {
       return false;
+    }
     TimeInstant other = (TimeInstant) obj;
-    if (myHour != other.myHour)
-      return false;
-    if (myMinute != other.myMinute)
-      return false;
-    return true;
+    return com.google.common.base.Objects.equal(myHour, other.myHour)
+        && com.google.common.base.Objects.equal(myMinute, other.myMinute);
   }
 }
