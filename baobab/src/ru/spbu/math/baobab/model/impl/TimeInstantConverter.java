59aca83fa64ab80a6313cf98176fe14d5c949188
Vita.Loginova <Vita.Loginova@localhost>
2012-11-06 00:13:13 +0400

    comments, format and cosmetic changes review: dluciv, dbarashev

--- baobab/src/ru/spbu/math/baobab/model/impl/TimeInstantConverter.java
+++ baobab/src/ru/spbu/math/baobab/model/impl/TimeInstantConverter.java
@@ -1,32 +1,33 @@
 package ru.spbu.math.baobab.model.impl;
 
-/**
- * converter for TimeInstant
- * 
- * @author vloginova
- */
-
 import ru.spbu.math.baobab.model.TimeInstant;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Converts time lexeme on Baobab language to convenient type
+ * 
+ * @author vloginova
+ */
+
 public class TimeInstantConverter {
-	
-	private final static Pattern TIME_PATTERN = Pattern.compile("(\\d{1,2}):(\\d{2})");
-	
-	/*
-	 * converts string to TimeInstant
-	 * 
-	 * @param value string to convert
-	 * @return converted TimeInstant
-	 */
-	public static TimeInstant convertToTimeInstant(String value) throws Exception{
-		Matcher commandMatch = TIME_PATTERN.matcher(value);
-		if (commandMatch.lookingAt()) {
-			int hour = Integer.parseInt(commandMatch.group(1));
-			int minute = Integer.parseInt(commandMatch.group(2));
-			return new TimeInstant(hour, minute);
-		}
-		throw new Exception("convertToTimeInstant: Incorrect value");
-	}
+
+  private final static Pattern TIME_PATTERN = Pattern.compile("(\\d{1,2}):(\\d{2})");
+
+  /**
+   * converts string to TimeInstant
+   * 
+   * @param value
+   *          string to convert
+   * @return converted TimeInstant
+   */
+  public static TimeInstant convertToTimeInstant(String value) throws Exception {
+    Matcher commandMatch = TIME_PATTERN.matcher(value);
+    if (commandMatch.matches()) {
+      int hour = Integer.parseInt(commandMatch.group(1));
+      int minute = Integer.parseInt(commandMatch.group(2));
+      return new TimeInstant(hour, minute);
+    }
+    throw new Exception("convertToTimeInstant: Incorrect value");
+  }
 }
