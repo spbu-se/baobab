b611070206857919a02a3ef8873457f20fe4d50e
usd.icq <usd.icq@localhost>
2012-11-10 02:09:32 +0400

    Changes in Attendee & AttendeeExtent SQL implementations. New constructor for Attendee, some modifications caused by the change of Attendee constructor. review: dbarashev, dluciv

--- baobab/src/ru/spbu/math/baobab/server/sql/AttendeeSqlImpl.java
+++ baobab/src/ru/spbu/math/baobab/server/sql/AttendeeSqlImpl.java
@@ -25,31 +25,13 @@ public class AttendeeSqlImpl implements Attendee {
   private final AttendeeExtent myExtent;
 
   // constructor
-  public AttendeeSqlImpl(int id, String uid, String name, Type type, AttendeeExtent extent) {
+  public AttendeeSqlImpl(int id, String uid, String name, Type type, Integer group_id, AttendeeExtent extent) {
     myID = id;
     myUID = uid;
     myName = name;
     myType = type;
-    myGroupId = null;
+    myGroupId = group_id;
     myExtent = extent;
-    // set group_id for group members
-    if (this.isGroup()) {
-      SqlApi con = SqlApi.create();
-      try {
-        List<PreparedStatement> stmt = con.prepareScript("INSERT INTO AttendeeGroup SET id=?; \n "
-            + "UPDATE Attendee SET group_id = ? WHERE id = ?;");
-        stmt.get(0).setInt(1, myID);
-        stmt.get(0).execute();
-        stmt.get(1).setInt(1, myID);
-        stmt.get(1).setInt(2, myID);
-        stmt.get(1).execute();
-        myGroupId = myID;
-      } catch (SQLException e) {
-        e.printStackTrace();
-      } finally {
-        con.dispose();
-      }
-    }
   }
 
   @Override
@@ -100,7 +82,7 @@ public class AttendeeSqlImpl implements Attendee {
     try {
       Collection<Attendee> members = new ArrayList<Attendee>();
       // find out data about members of this group
-      String query = "SELECT A_member.id, A_member.uid, A_member.name, A_member.type "
+      String query = "SELECT A_member.id, A_member.uid, A_member.name, A_member.type, A_member.group_id"
           + "FROM Attendee A_group JOIN AttendeeGroup AG ON (A_group.group_id = AG.id) "
           + "JOIN GroupMember GM ON (GM.group_id = AG.id) JOIN Attendee A_member "
           + "ON (A_member.id = GM.attendee_id) WHERE A_group.id = ?;";
@@ -108,12 +90,17 @@ public class AttendeeSqlImpl implements Attendee {
       stmt.get(0).setInt(1, myID);
       ResultSet result = stmt.get(0).executeQuery();
       while (result.next()) {
-        int id = result.getInt(1);
-        String uid = result.getString(2);
-        String name = result.getString(3);
-        Type type = Type.values()[result.getInt(4)];
-        Attendee groupAttendee = new AttendeeSqlImpl(id, uid, name, type, myExtent);
-        members.add(groupAttendee);
+        int group_id = result.getInt("A_member.group_id");
+        Type type = Type.values()[result.getInt("A_member.type")];
+        if ((group_id == 0) && (type != Type.ACADEMIC_GROUP) && (type == Type.CHAIR) && (type == Type.FREE_FORM_GROUP)) {
+          Attendee groupAttendee = new AttendeeSqlImpl(result.getInt("A_member.id"), result.getString("A_member.uid"),
+              result.getString("A_member.name"), type, null, myExtent);
+          members.add(groupAttendee);
+        } else {
+          Attendee groupAttendee = new AttendeeSqlImpl(result.getInt("A_member.id"), result.getString("A_member.uid"),
+              result.getString("A_member.name"), type, group_id, myExtent);
+          members.add(groupAttendee);
+        }
       }
       return members;
     } catch (SQLException e) {
@@ -138,7 +125,7 @@ public class AttendeeSqlImpl implements Attendee {
       ResultSet result = stmt.get(0).executeQuery();
       result.next();
       stmt.get(1).setInt(1, myGroupId); // set group_id
-      stmt.get(1).setInt(2, result.getInt(1)); // set attendee_id
+      stmt.get(1).setInt(2, result.getInt("id")); // set attendee_id
       stmt.get(1).execute();
     } catch (SQLException e) {
       e.printStackTrace();
