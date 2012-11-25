15838a6b89a07ead4ce37c6690829744fb87e5e1
dnsjkee <dnsjkee@localhost>
2012-11-25 20:12:11 +0400

    Implementation of 'create auditorium' command interpreter review: dbarashev, dluciv

--- baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
+++ baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
@@ -2,6 +2,8 @@ package ru.spbu.math.baobab.lang;
 
 import java.util.Arrays;
 import java.util.List;
+
+import ru.spbu.math.baobab.server.AuditoriumExtentImpl;
 import ru.spbu.math.baobab.server.TimeSlotExtentImpl;
 
 /**
@@ -10,7 +12,9 @@ import ru.spbu.math.baobab.server.TimeSlotExtentImpl;
  * @author ragozina.anastasiya, vloginova
  */
 public class ScriptInterpreter {
-  private final List<Parser> parsers = Arrays.asList((Parser) new TimeSlotCommandParser(new TimeSlotExtentImpl()));
+  private final List<Parser> tsParsers = Arrays.asList((Parser) new TimeSlotCommandParser(new TimeSlotExtentImpl()));
+  private final List<Parser> auditoriumParsers = Arrays.asList((Parser) new AuditoriumCommandParser(new AuditoriumExtentImpl()));
+  
 
   /**
    * Receive command and process it
@@ -18,7 +22,12 @@ public class ScriptInterpreter {
    * @param command some command in baobab language
    */
   public void process(String command) {
-    for (Parser p : parsers) {
+    for (Parser p : tsParsers) {
+      if (p.parse(command)) {
+        return;
+      }
+    }
+    for (Parser p : auditoriumParsers) {
       if (p.parse(command)) {
         return;
       }
