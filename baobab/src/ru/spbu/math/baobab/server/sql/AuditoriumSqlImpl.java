1d95959da3c111e3e3c5a6b07a2ce97284b04577
Vita.Loginova <Vita.Loginova@localhost>
2012-11-25 21:24:43 +0400

    AuditoriumExtentSqlImpl, AuditoriumSqlImpl and tests for them review: dluciv, dbarashev

---
+++ baobab/src/ru/spbu/math/baobab/server/sql/AuditoriumSqlImpl.java
@@ -0,0 +1,51 @@
+package ru.spbu.math.baobab.server.sql;
+
+import com.google.common.base.Objects;
+
+import ru.spbu.math.baobab.model.Auditorium;
+
+/**
+ * SQL-based implementation of Auditorium
+ * 
+ * @author vloginova
+ */
+public class AuditoriumSqlImpl implements Auditorium {
+
+  private String myID;
+  private int myCapacity;
+
+  public AuditoriumSqlImpl(String id, int capacity) {
+    myID = id;
+    myCapacity = capacity;
+  }
+
+  @Override
+  public String getID() {
+    return myID;
+  }
+
+  @Override
+  public int getCapacity() {
+    return myCapacity;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(myID, myCapacity);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+    if (obj instanceof Auditorium == false) {
+      return false;
+    }
+    Auditorium auditorium = (Auditorium) obj;
+    if (this.getID().equals(auditorium.getID())) {
+      return true;
+    }
+    return false;
+  }
+}
