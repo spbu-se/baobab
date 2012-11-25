35a34a0e76ddfd5813eb6b8d7ced0b8436a953aa
usd.icq <usd.icq@localhost>
2012-11-26 01:27:18 +0400

    Draft of a web-view of a calendar of a single attendee. review: dbarashev, dluciv

---
+++ baobab/src/ru/spbu/math/baobab/server/CalendarOfAttendee.java
@@ -0,0 +1,67 @@
+package ru.spbu.math.baobab.server;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import ru.spbu.math.baobab.model.TimeSlot;
+
+/**
+ * CalendarOfAttendee can create table which represents a calendar of a single attendee
+ * 
+ * @author aoool
+ */
+public class CalendarOfAttendee {
+  private final List<TimeSlot> myHorHeaders;
+  private final List<String> myVertHeaders;
+  private final static String[] myDaysOfWeek = { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+  private final static TableCell EMPTY_CELL = new TableCell("");
+
+  public CalendarOfAttendee(List<TimeSlot> timeSlots) {
+    myHorHeaders = timeSlots;
+    List<Integer> arrayOfDays = Lists.newArrayList();
+    for (TimeSlot timeslot : myHorHeaders) {
+      if (!arrayOfDays.contains(timeslot.getDayOfWeek())) {
+        arrayOfDays.add(timeslot.getDayOfWeek());
+      }
+    }
+    myVertHeaders = getSortedDaysOfWeek(arrayOfDays);
+  }
+
+  private List<String> getSortedDaysOfWeek(List<Integer> arrayOfDays) {
+    List<String> sortedDaysOfWeek = Lists.newArrayList();
+    while (!arrayOfDays.isEmpty()) {
+      int min = 0;
+      for (int i = 0; i < arrayOfDays.size(); i++) {
+        if (arrayOfDays.get(min) > arrayOfDays.get(i)) {
+          min = i;
+        }
+      }
+      sortedDaysOfWeek.add(myDaysOfWeek[arrayOfDays.get(min) - 1]);
+      arrayOfDays.remove(min);
+    }
+    return sortedDaysOfWeek;
+  }
+
+  /**
+   * @return List of {@link TableRow} which represents a calendar of a single attendee as a table.
+   */
+  public List<TableRow> getTableRows() {
+    List<TableRow> table = Lists.newArrayList();
+    TableRow row = new TableRow();
+    row.addCell(EMPTY_CELL);
+    for (int i = 0; i < myVertHeaders.size(); i++) {
+      row.addCell(new TableCell(myVertHeaders.get(i)));
+    }
+    table.add(row);
+    for (int i = 0; i < myHorHeaders.size(); i++) {
+      row = new TableRow();
+      row.addCell(new TableCell(myHorHeaders.get(i).getName()));
+      for (int j = 0; j < myVertHeaders.size(); j++) {
+        row.addCell(EMPTY_CELL);
+      }
+      table.add(row);
+    }
+    return table;
+  }
+}
