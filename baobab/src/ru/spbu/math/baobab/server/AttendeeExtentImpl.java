bd1d8e54130ffd3209d4686c498f4a54c182f873
Dmitry Barashev <dmitry@barashev.net>
2012-11-28 16:40:47 +0400

    Merge with 0678de0494512db54ab8f507c40ec5e80322d011

---
+++ baobab/src/ru/spbu/math/baobab/server/AttendeeExtentImpl.java
@@ -0,0 +1,31 @@
+package ru.spbu.math.baobab.server;
+
+import java.util.HashMap;
+import static com.google.common.collect.Maps.newHashMap;
+
+import ru.spbu.math.baobab.model.Attendee;
+import ru.spbu.math.baobab.model.Attendee.Type;
+import ru.spbu.math.baobab.model.AttendeeExtent;
+
+/**
+ * Implementation of AttendeeExtent interface
+ * 
+ * @author agudulin
+ */
+public class AttendeeExtentImpl implements AttendeeExtent {
+  private final HashMap<String, Attendee> myAttendees = newHashMap();
+  
+  @Override
+  public Attendee create(String id, String name, Type type) {
+    Attendee attendee = new AttendeeImpl(id, name, type, this);
+    // Constructor will throw IllegalArgumentException if 
+    // an attendee with current id already exists
+    myAttendees.put(id, attendee);
+    return attendee;
+  }
+
+  @Override
+  public Attendee find(String id) {
+    return myAttendees.get(id);
+  }
+}
