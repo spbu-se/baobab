87ebd632ec19d6fceb7ba38ff4d7f685f94fe01f
usd.icq <usd.icq@localhost>
2012-11-28 21:30:16 +0400

    Adding name of attendee those schedule we print and changes in the way of sort array. review: dbarashev, dluciv

--- baobab/src/ru/spbu/math/baobab/server/CalendarOfAttendeeServlet.java
+++ baobab/src/ru/spbu/math/baobab/server/CalendarOfAttendeeServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import ru.spbu.math.baobab.model.Attendee;
 import ru.spbu.math.baobab.model.TimeSlot;
 
 /**
@@ -23,10 +24,12 @@ public abstract class CalendarOfAttendeeServlet extends HttpServlet {
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     request.setCharacterEncoding("UTF-8");
     CalendarOfAttendee table = new CalendarOfAttendee(createTimeSlots());
+    request.setAttribute("AttendeeID", createAttendee().getID());
     request.setAttribute("tableRows", table.getTableRows());
     RequestDispatcher view = request.getRequestDispatcher("/calendar_of_attendee.jsp");
     view.forward(request, response);
   }
 
   protected abstract List<TimeSlot> createTimeSlots();
+  protected abstract Attendee createAttendee();
 }
