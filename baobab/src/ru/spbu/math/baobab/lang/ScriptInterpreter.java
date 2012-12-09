0604930ddce3d2e117775c14491e1b0a32500f46
Vita.Loginova <Vita.Loginova@localhost>
2012-12-09 20:29:29 +0400

    small refactoring review: dluciv, dbarashev

--- baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
+++ baobab/src/ru/spbu/math/baobab/lang/ScriptInterpreter.java
@@ -2,8 +2,9 @@ package ru.spbu.math.baobab.lang;
 
 import java.util.Arrays;
 import java.util.List;
+
+import ru.spbu.math.baobab.server.AuditoriumExtentImpl;
 import ru.spbu.math.baobab.server.TimeSlotExtentImpl;
-import ru.spbu.math.baobab.server.AttendeeExtentImpl;
 
 /**
  * Script interpreter processes the lines satisfying specifications of the Baobab language and executes them
@@ -11,16 +12,23 @@ import ru.spbu.math.baobab.server.AttendeeExtentImpl;
  * @author ragozina.anastasiya, vloginova
  */
 public class ScriptInterpreter {
-  private final List<Parser> parsers = Arrays.asList(new TimeSlotCommandParser(new TimeSlotExtentImpl()),
-      new AttendeeCommandParser(new AttendeeExtentImpl()));
+  private final List<Parser> myParsers;
 
+  public ScriptInterpreter() {
+    this(Arrays.asList(new TimeSlotCommandParser(new TimeSlotExtentImpl()), new AuditoriumCommandParser(new AuditoriumExtentImpl())));
+  }
+  
+  public ScriptInterpreter(List<Parser> parsers) {
+    myParsers = parsers;
+  }
+  
   /**
    * Receive command and process it
    * 
    * @param command some command in baobab language
    */
   public void process(String command) {
-    for (Parser p : parsers) {
+    for (Parser p : myParsers) {
       if (p.parse(command)) {
         return;
       }
@@ -28,3 +36,4 @@ public class ScriptInterpreter {
     throw new IllegalArgumentException("Incorrect command");
   }
 }
+
