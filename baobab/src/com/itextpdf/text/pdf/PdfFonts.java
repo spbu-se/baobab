80a3b19f50e11e3bd3d2bbead322501b4177f074
Dmitry Barashev <dmitry@barashev.net>
2012-12-19 15:40:54 +0400

    updated PDF schedule. Attendee grouping, automated "typical" timeslot calculation
    review: abondarev

--- baobab/src/com/itextpdf/text/pdf/PdfFonts.java
+++ baobab/src/com/itextpdf/text/pdf/PdfFonts.java
@@ -16,18 +16,18 @@ public class PdfFonts {
 
   public static Font CONSULT_LABEL;
   public static Font TEACHER_LABEL;
+  public static final BaseColor GREY = new BaseColor(0x666666);
   
   static {
     try {
-      BaseColor grey = new BaseColor(0x666666);
       byte[] fontBytes = Resources.toByteArray(Resources.getResource("LiberationSans-Regular.ttf"));
       TrueTypeFontUnicode ttfFont = new TrueTypeFontUnicode("LiberationSans-Regular.ttf", BaseFont.IDENTITY_H, true, fontBytes, false);
       BIG_FONT = new Font(ttfFont, 30, Font.NORMAL, new BaseColor(0, 0, 0));
       MEDIUM_FONT = new Font(ttfFont, 16, Font.NORMAL, new BaseColor(0, 0, 0));
       SMALL_FONT = new Font(ttfFont, 10, Font.NORMAL, new BaseColor(0, 0, 0));
       
-      CONSULT_LABEL = new Font(ttfFont, 10, Font.NORMAL, grey);
-      TEACHER_LABEL = new Font(ttfFont, 16, Font.NORMAL, grey);
+      CONSULT_LABEL = new Font(ttfFont, 10, Font.NORMAL, GREY);
+      TEACHER_LABEL = new Font(ttfFont, 16, Font.NORMAL, GREY);
     } catch (IOException e) {
       // TODO Auto-generated catch block
       e.printStackTrace();
