0e41fd0a82cb2ba63acf310212651a26bc5ce6fb
usd.icq <usd.icq@localhost>
2013-03-20 01:08:42 +0400

    Some changes in UserProfile model. review: dbarashev

--- baobab/src/ru/spbu/math/baobab/model/UserProfile.java
+++ baobab/src/ru/spbu/math/baobab/model/UserProfile.java
@@ -2,47 +2,49 @@ package ru.spbu.math.baobab.model;
 
 /**
  * UserProfile instance manages personal information about single Attendee (in case attendee provides this information).
- * Signed-in users can add or change the following information: 
- *    1. Membership in academic group if user is a student 
- *    2. Department if user is a teacher
- *    3. Email/XMPP for notifications.
+ * Signed-in users can add or change the following information: 1. Membership in academic group if user is a student; 2.
+ * Department if user is a teacher; 3. Email/XMPP for notifications.
  * 
  * @author aoool
  */
 public interface UserProfile {
-  
+
   /**
    * Sets or changes user's membership in Academic Group.
+   * 
    * @param academicGroup Attendee with a type == ACADEMIC_GROUP
    * @throws IllegalArgumentException - if attendee's type != ACADEMIC_GROUP
    */
   public void setAcademicGroup(Attendee academicGroup);
-  
+
   /**
-   * Gets Academic Group UID in which user has a membership.
+   * @return attendee with type == ACADEMIC_GROUP otherwise null
    */
-  public String getAcademicGroupUid();
-  
+  public Attendee getAcademicGroup();
+
   /**
    * Sets or changes user's membership in Chair.
+   * 
    * @param chair Attendee with a type == CHAIR
    * @throws IllegalArgumentException - if attendee's type != CHAIR
    */
   public void setChair(Attendee chair);
-  
+
   /**
-   * Gets Chair UID in which user has membership.
+   * @return attendee with type == CHAIR otherwise null
    */
-  public String getChairUid();
-  
+  public Attendee getChair();
+
   /**
    * Sets or changes user's email/XMPP.
+   * 
    * @param email User's email or XMPP
+   * @throws IllegalArgumentException if entered email or XMPP was wrong
    */
   public void setEmail(String email);
-  
+
   /**
-   * Gets user's email or XMPP.
+   * @return user's email or XMPP
    */
   public String getEmail();
 }
